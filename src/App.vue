6854ffb56f9bf298c3491b52e341c17707204fa4
AhmedWaleed <ahmed_waleed1@hotmail.com>
2020-09-29 20:11:02 +0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,268 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <banner></banner>
+
+    <main class="container mx-auto my-16">
+      <section class="how-it-works space-y-12 bg-white">
+        <section class="requester space-y-12">
+          <h2 class="font-bold text-3xl text-center">Are you a requester? how it works:</h2>
+          <div class="flex items-center">
+            <div class="flex flex-col items-center space-y-8 space-x-2 text-center">
+              <img class="w-40 h-40 rounded-full border-4 border-gray-700" src="http://lorempixel.com/640/480/business" alt="image ">
+              <p class="text-xs text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, laboriosam.</p>  
+            </div>
+            <div class="flex flex-col items-center space-y-8 space-x-2 text-center">
+              <img class="w-40 h-40 rounded-full border-4 border-gray-700" src="http://lorempixel.com/640/480/fashion" alt="image ">
+              <p class="text-xs text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, laboriosam.</p>  
+            </div>
+            <div class="flex flex-col items-center space-y-8 space-x-2 text-center">
+              <img class="w-40 h-40 rounded-full border-4 border-gray-700" src="http://lorempixel.com/640/480/transport" alt="image ">
+              <p class="text-xs text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, laboriosam.</p>  
+            </div>
+            <div class="flex flex-col items-center space-y-8 space-x-2 text-center">
+              <img class="w-40 h-40 rounded-full border-4 border-gray-700" src="http://lorempixel.com/640/480/city" alt="image ">
+              <p class="text-xs text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, laboriosam.</p>  
+            </div>
+          </div>
+          <div class="flex justify-center font-semibold space-x-8">
+            <button class="bg-green-500 text-white focus:outline-none hover:bg-green-600 px-4 w-48 py-4 uppercase rounded-md">Post a Request</button>
+            <button class="border text-green-500 border-green-500 focus:outline-none  hover:bg-green-500 w-48 hover:text-white  px-4 py-4 uppercase rounded-md">Watch Video</button>
+          </div>
+        </section>
+        <section class="traveler space-y-12">
+          <h2 class="font-bold text-3xl text-center">Are you a Traveler? how it works:</h2>
+          <div class="flex items-center">
+            <div class="flex flex-col items-center space-y-8 space-x-2 text-center">
+              <img class="w-40 h-40 rounded-full border-4 border-gray-700" src="http://lorempixel.com/640/480/cats" alt="image ">
+              <p class="text-xs text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, laboriosam.</p>  
+            </div>
+            <div class="flex flex-col items-center space-y-8 space-x-2 text-center">
+              <img class="w-40 h-40 rounded-full border-4 border-gray-700" src="http://lorempixel.com/640/480/people" alt="image ">
+              <p class="text-xs text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, laboriosam.</p>  
+            </div>
+            <div class="flex flex-col items-center space-y-8 space-x-2 text-center">
+              <img class="w-40 h-40 rounded-full border-4 border-gray-700" src="http://lorempixel.com/640/480/nature" alt="image ">
+              <p class="text-xs text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, laboriosam.</p>  
+            </div>
+            <div class="flex flex-col items-center space-y-8 space-x-2 text-center">
+              <img class="w-40 h-40 rounded-full border-4 border-gray-700" src="http://lorempixel.com/640/480/animals" alt="image ">
+              <p class="text-xs text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, laboriosam.</p>  
+            </div>
+          </div>
+          <div class="flex justify-center font-semibold space-x-8">
+            <button class="bg-green-500 text-white focus:outline-none hover:bg-green-600 px-4 w-48 py-4 uppercase rounded-md">Post a Trip</button>
+            <button class="border text-green-500 border-green-500 focus:outline-none  hover:bg-green-500 w-48 hover:text-white  px-4 py-4 uppercase rounded-md">Watch Video</button>
+          </div>
+        </section>
+        <section class="be-a-requester space-y-12">
+          <h2 class="font-bold text-3xl text-center">Be a Requester and...</h2>
+          <div class="flex space-x-24 justify-center">
+            <div class="flex border border-gray-900 rounded-md p-4 w-1/5">
+              <div class="flex flex-col space-y-6 items-center text-center">
+                <img class="border-4 border-gray-700 w-24 h-24 rounded-full" src="http://lorempixel.com/640/480/nature" alt="image">
+                <p class="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, veniam!</p>
+              </div>
+            </div>
+            <div class="flex border border-gray-900 rounded-md p-4 w-1/5">
+              <div class="flex flex-col space-y-6 items-center text-center">
+                <img class="border-4 border-gray-700 w-24 h-24 rounded-full" src="http://lorempixel.com/640/480/transport" alt="image">
+                <p class="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, veniam!</p>
+              </div>
+            </div>
+            <div class="flex border border-gray-900 rounded-md p-4 w-1/5">
+              <div class="flex flex-col space-y-6 items-center text-center">
+                <img class="border-4 border-gray-700 w-24 h-24 rounded-full" src="http://lorempixel.com/640/480/abstract" alt="image">
+                <p class="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, veniam!</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="be-a-traveler space-y-12">
+          <h2 class="font-bold text-3xl text-center">Be a Traveler and...</h2>
+          <div class="flex space-x-24 justify-center">
+            <div class="flex border border-gray-900 rounded-md p-4 w-1/5">
+              <div class="flex flex-col space-y-6 items-center text-center">
+                <img class="border-4 border-gray-700 w-24 h-24 rounded-full" src="http://lorempixel.com/640/480/nature" alt="image">
+                <p class="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, veniam!</p>
+              </div>
+            </div>
+            <div class="flex border border-gray-900 rounded-md p-4 w-1/5">
+              <div class="flex flex-col space-y-6 items-center text-center">
+                <img class="border-4 border-gray-700 w-24 h-24 rounded-full" src="http://lorempixel.com/640/480/fashion" alt="image">
+                <p class="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, veniam!</p>
+              </div>
+            </div>
+            <div class="flex border border-gray-900 rounded-md p-4 w-1/5">
+              <div class="flex flex-col space-y-6 items-center text-center">
+                <img class="border-4 border-gray-700 w-24 h-24 rounded-full" src="http://lorempixel.com/640/480/business" alt="image">
+                <p class="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, veniam!</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
+    </main>
+
+    <section class="recent-posts p-6 bg-gray-300 mt-8 flex text-center">
+        <div class="w-3/6 space-y-12">
+          <h2 class="text-2xl font-semibold">Recently Posted Requests</h2>
+          <div class="flex flex-col items-center space-y-8">
+            <div class="p-4 w-4/5 bg-white border-l-8 border-yellow-600 rounded-md shadow-lg">
+            <div class="flex justify-between">
+              <a class="flex items-center space-x-2" href="#">
+                <img class="rounded-full w-12 h-12" src="https://s3.amazonaws.com/uifaces/faces/twitter/arpitnj/128.jpg" alt="avatar">
+                <span class="text-green-500 font-semibold">username</span>
+              </a>
+              <ul class="flex item-center space-x-2">
+                <li><a href="#"></a><img class="rounded-full w-5 h-5" src="https://s3.amazonaws.com/uifaces/faces/twitter/lhausermann/128.jpg" alt="avatar"></li>
+                <li><a href="#"></a><img class="rounded-full w-5 h-5" src="https://s3.amazonaws.com/uifaces/faces/twitter/axel/128.jpg" alt="avatar"></li>
+                <li><a href="#"></a><img class="rounded-full w-5 h-5" src="https://s3.amazonaws.com/uifaces/faces/twitter/mbilderbach/128.jpg" alt="avatar"></li>
+                <li><a href="#"></a><img class="rounded-full w-5 h-5" src="https://s3.amazonaws.com/uifaces/faces/twitter/vivekprvr/128.jpg" alt="avatar"></li>
+              </ul>
+            </div>
+            <div class="flex">
+              <div class="w-1/5 mt-4">
+                <ul class="flex flex-col space-y-2 text-gray-600 text-xs">
+                  <li class="flex items-center space-x-2 space-y-2"><img class="w-4 h-4" src="http://lorempixel.com/640/480/city" alt="image"><span>0 offers sent</span></li>
+                  <li class="flex items-center space-x-2 space-y-2"><img class="w-4 h-4" src="http://lorempixel.com/640/480/transport" alt="image"><span>3 items</span></li>
+                  <li class="flex items-center space-x-2 space-y-2"><img class="w-4 h-4" src="http://lorempixel.com/640/480/abstract" alt="image"><span>RS 10,000</span></li>
+                  <li class="flex items-center space-x-2 space-y-2"><img class="w-4 h-4" src="http://lorempixel.com/640/480/nightlife" alt="image"><span>5.0</span></li>
+                </ul>
+            </div>
+            <div class="w-4/5 flex flex-col mt-4">
+              <div class="flex items-center justify-center space-x-2 text-gray-800 font-semibold">
+                <div>Washington, USA</div>
+                <img class="w-8 h-8" src="http://lorempixel.com/640/480/nature" alt="image">
+                <div>Lahore, Pakistan</div>
+              </div>
+              <div class="flex justify-end space-x-12 mt-10">
+                <h2 class="text-4xl text-green-500 font-semibold">Rs. 2,500</h2>
+                <button class="px-3 py-3 bg-green-500 hover:bg-green-600 focus:outline-none text-white rounded-md mr-0">Send an offer</button>
+              </div>
+            </div>
+            </div>
+          </div>
+          <div class="p-4 w-4/5 bg-white border-l-8 border-yellow-600 rounded-md shadow-lg">
+            <div class="flex justify-between">
+              <a class="flex items-center space-x-2" href="#">
+                <img class="rounded-full w-12 h-12" src="https://s3.amazonaws.com/uifaces/faces/twitter/mikaeljorhult/128.jpg" alt="avatar">
+                <span class="text-green-500 font-semibold">username</span>
+              </a>
+              <ul class="flex item-center space-x-2">
+                <li><a href="#"></a><img class="rounded-full w-5 h-5" src="https://s3.amazonaws.com/uifaces/faces/twitter/lhausermann/128.jpg" alt="avatar"></li>
+                <li><a href="#"></a><img class="rounded-full w-5 h-5" src="https://s3.amazonaws.com/uifaces/faces/twitter/axel/128.jpg" alt="avatar"></li>
+                <li><a href="#"></a><img class="rounded-full w-5 h-5" src="https://s3.amazonaws.com/uifaces/faces/twitter/mbilderbach/128.jpg" alt="avatar"></li>
+                <li><a href="#"></a><img class="rounded-full w-5 h-5" src="https://s3.amazonaws.com/uifaces/faces/twitter/vivekprvr/128.jpg" alt="avatar"></li>
+              </ul>
+            </div>
+            <div class="flex">
+              <div class="w-1/5 mt-4">
+                <ul class="flex flex-col space-y-2 text-gray-600 text-xs">
+                  <li class="flex items-center space-x-2 space-y-2"><img class="w-4 h-4" src="http://lorempixel.com/640/480/transport" alt="image"><span>0 offers sent</span></li>
+                  <li class="flex items-center space-x-2 space-y-2"><img class="w-4 h-4" src="http://lorempixel.com/640/480/sports" alt="image"><span>3 items</span></li>
+                  <li class="flex items-center space-x-2 space-y-2"><img class="w-4 h-4" src="http://lorempixel.com/640/480/abstract" alt="image"><span>RS 10,000</span></li>
+                  <li class="flex items-center space-x-2 space-y-2"><img class="w-4 h-4" src="http://lorempixel.com/640/480/cats" alt="image"><span>5.0</span></li>
+                </ul>
+            </div>
+            <div class="w-4/5 flex flex-col mt-4">
+              <div class="flex items-center justify-center space-x-2 text-gray-800 font-semibold">
+                <div>Washington, USA</div>
+                <img class="w-8 h-8" src="http://lorempixel.com/640/480/animals" alt="image">
+                <div>Lahore, Pakistan</div>
+              </div>
+              <div class="flex justify-end space-x-12 mt-10">
+                <h2 class="text-4xl text-green-500 font-semibold">Rs. 2,500</h2>
+                <button class="px-3 py-3 bg-green-500 hover:bg-green-600 focus:outline-none text-white rounded-md mr-0">Send an offer</button>
+              </div>
+            </div>
+            </div>
+          </div>
+          </div>
+          <button class="w-48 px-6 py-4 bg-green-500 hover:bg-green-600 rounded-md focus:outline-none text-white">View All Requests</button>
+        </div>
+        <div class="w-3/6 space-y-12">
+          <h2 class="text-2xl font-semibold">Recently Posted Trips</h2>
+          <div class="flex flex-col items-center space-y-8">
+            <div class="p-4 w-4/5 rounded-md shadow-lg bg-local text-white" style="background-image: url('https://images.unsplash.com/photo-1485738422979-f5c462d49f74?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')">
+            <div class="flex justify-between">
+              <a class="flex items-center space-x-2" href="#">
+                <img class="rounded-full w-12 h-12" src="https://s3.amazonaws.com/uifaces/faces/twitter/chatyrko/128.jpg" alt="avatar">
+                <span class="font-semibold">username</span>
+              </a>
+              <ul class="flex item-center space-x-2">
+                <li><a href="#"></a><img class="rounded-full w-5 h-5" src="https://s3.amazonaws.com/uifaces/faces/twitter/lhausermann/128.jpg" alt="avatar"></li>
+                <li><a href="#"></a><img class="rounded-full w-5 h-5" src="https://s3.amazonaws.com/uifaces/faces/twitter/axel/128.jpg" alt="avatar"></li>
+                <li><a href="#"></a><img class="rounded-full w-5 h-5" src="https://s3.amazonaws.com/uifaces/faces/twitter/mbilderbach/128.jpg" alt="avatar"></li>
+                <li><a href="#"></a><img class="rounded-full w-5 h-5" src="https://s3.amazonaws.com/uifaces/faces/twitter/mbilderbach/128.jpg" alt="avatar"></li>
+              </ul>
+            </div>
+            <div class="flex">
+              <div class="w-1/5 mt-4">
+                <ul class="flex flex-col space-y-6 text-xs">
+                  <li class="flex items-center space-x-2 space-y-2"><img class="w-4 h-4" src="http://lorempixel.com/640/480/animals" alt="image"><span>10th Jul, 2020</span></li>
+                  <li class="flex items-center space-x-2 space-y-2"><img class="w-4 h-4" src="http://lorempixel.com/640/480/abstract" alt="image"><span>Max Rs. 5,00</span></li>
+                  <li class="flex items-center space-x-2 space-y-2"><img class="w-4 h-4" src="http://lorempixel.com/640/480/food" alt="image"><span>Not rated yet</span></li>
+                </ul>
+            </div>
+            <div class="w-4/5 flex flex-col mt-4">
+              <div class="flex items-center justify-center space-x-2 font-semibold">
+                <div>Washington, USA</div>
+                <img class="w-8 h-8" src="http://lorempixel.com/640/480/transport" alt="image">
+                <div>Lahore, Pakistan</div>
+              </div>
+              <div class="flex justify-end space-x-12 mt-10">
+                <button class="px-3 py-3 bg-white text-green-500 focus:outline-none hover:bg-gray-100 rounded-md mr-0">Send a request</button>
+              </div>
+            </div>
+            </div>
+          </div>
+          <div class="p-4 w-4/5 rounded-md shadow-lg bg-local text-white" style="background-image: url('https://images.unsplash.com/photo-1485738422979-f5c462d49f74?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')">
+            <div class="flex justify-between">
+              <a class="flex items-center space-x-2" href="#">
+                <img class="rounded-full w-12 h-12" src="https://s3.amazonaws.com/uifaces/faces/twitter/eyronn/128.jpg" alt="avatar">
+                <span class="font-semibold">username</span>
+              </a>
+              <ul class="flex item-center space-x-2">
+                <li><a href="#"></a><img class="rounded-full w-5 h-5" src="https://s3.amazonaws.com/uifaces/faces/twitter/lhausermann/128.jpg" alt="avatar"></li>
+                <li><a href="#"></a><img class="rounded-full w-5 h-5" src="https://s3.amazonaws.com/uifaces/faces/twitter/axel/128.jpg" alt="avatar"></li>
+                <li><a href="#"></a><img class="rounded-full w-5 h-5" src="https://s3.amazonaws.com/uifaces/faces/twitter/mbilderbach/128.jpg" alt="avatar"></li>
+                <li><a href="#"></a><img class="rounded-full w-5 h-5" src="https://s3.amazonaws.com/uifaces/faces/twitter/mbilderbach/128.jpg" alt="avatar"></li>
+              </ul>
+            </div>
+            <div class="flex">
+              <div class="w-1/5 mt-4">
+                <ul class="flex flex-col space-y-6 text-xs">
+                  <li class="flex items-center space-x-2 space-y-2"><img class="w-4 h-4" src="http://lorempixel.com/640/480/animals" alt="image"><span>10th Jul, 2020</span></li>
+                  <li class="flex items-center space-x-2 space-y-2"><img class="w-4 h-4" src="http://lorempixel.com/640/480/cats" alt="image"><span>Max Rs. 5,00</span></li>
+                  <li class="flex items-center space-x-2 space-y-2"><img class="w-4 h-4" src="http://lorempixel.com/640/480/business" alt="image"><span>Not rated yet</span></li>
+                </ul>
+            </div>
+            <div class="w-4/5 flex flex-col mt-4">
+              <div class="flex items-center justify-center space-x-2 font-semibold">
+                <div>Washington, USA</div>
+                <img class="w-8 h-8" src="http://lorempixel.com/640/480/people" alt="image">
+                <div>Lahore, Pakistan</div>
+              </div>
+              <div class="flex justify-end space-x-12 mt-10">
+                <button class="px-3 py-3 bg-white text-green-500 focus:outline-none hover:bg-gray-100 rounded-md mr-0">Send a request</button>
+              </div>
+            </div>
+            </div>
+          </div>
+          </div>
+          <button class="w-48 px-6 py-4 bg-green-500 hover:bg-green-600 rounded-md focus:outline-none text-white">View All Trip</button>
+        </div>
+      </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Banner from './components/Banner';
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Banner
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,63 @@
+<template>
+    <header class="bg-gray-700 h-screen text-white">
+      <div class="container mx-auto">
+        <nav class="p-4 lg:p-0 lg:py-6">
+          <div class="flex items-center justify-between">
+            <a href="#" class="flex items-center space-x-2">
+              <img class="w-12 h-12 lg:w-20 lg:h-20 rounded-full" src="https://s3.amazonaws.com/uifaces/faces/twitter/derekcramer/128.jpg" alt="avatar"/>
+              <span class="font-semibold text-2xl">shoppefly</span>
+            </a>
+  
+            <div class="lg:hidden">
+              <svg v-if="!isOpen" @click="isOpen = !isOpen" class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+              <svg v-if="isOpen" @click="isOpen = !isOpen" class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+            </div>
+  
+            <div class="hidden lg:flex items-center space-x-12">
+              <ul class="flex items-center space-x-4">
+                <li><a href="#">Requesting</a></li>
+                <li><a href="#">Traveling</a></li>
+                <li><a href="#">How it works</a></li>
+              </ul>
+              <input class="bg-white w-64 px-4 py-3 rounded-md placeholder-gray-600 focus:outline-none" type="text" placeholder="Search for users or items">
+              <button class="px-8 py-3 font-bold rounded-md uppercase focus:outline-none bg-green-500 hover:bg-green-600">Login</button>
+            </div>
+          </div>
+  
+          <div v-show="isOpen" class="lg:hidden">
+            <div class="border border-t border-gray-600 mt-6"></div>
+            <ul class="lg:hidden flex flex-col space-y-2 mt-6">
+                <li><a class="hover:text-gray-500" href="#">Requesting</a></li>
+                <li><a class="hover:text-gray-500" href="#">Traveling</a></li>
+                <li><a class="hover:text-gray-500" href="#">How it works</a></li>
+                <li><input class="text-xs px-4 py-2 bg-white w-full lg:w-64 rounded placeholder-gray-600 focus:outline-none" type="text" placeholder="Search for users or items"></li>
+                <li><button class="text-xs px-4 py-2 font-bold rounded uppercase focus:outline-none bg-green-500 hover:bg-green-600 w-full">Login</button></li>
+            </ul>
+          </div>
+        </nav>
+
+        <div class="text-center space-y-6 lg:space-y-12 mt-20">
+          <h1 class="text-2xl lg:text-6xl font-semibold text-center">We Belive. We Say. We Delivery</h1>
+          <p class="text-base lg:text-2xl text-center">Let the travelers deliver you anything you desire from everywhere around the globe.</p>
+          <div class="flex items-center justify-center font-semibold text-xs lg:text-base space-x-4 lg:space-x-24">
+            <button class="bg-green-500 focus:outline-none hover:bg-green-600  w-40 lg:w-64 px-6 py-4 lg:px-16 lg:py-6 uppercase rounded-md">Sign Up</button>
+            <button class="border border-white focus:outline-none hover:bg-gray-600 w-40 px-6 lg:w-64 py-4 lg:px-16 lg:py-6 uppercase rounded-md">Virtual Tour</button>
+          </div>
+        </div>
+      </div>
+    </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
